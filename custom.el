1fdc822f2917a6f4ae20084f570584bc5bb00ad7
Rob Streeting <robsws@gmail.com>
2023-02-12 13:27:39 +0000

    Update custom.el

--- custom.el
+++ custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(company flycheck lsp-ui lsp-mode rust-mode python-mode magit haskell-mode haskell-emacs use-package)))
+   '(perspective ## markdown-preview-mode terraform-mode multiple-cursors yaml-mode verb request gruvbox-theme company flycheck lsp-ui lsp-mode rust-mode python-mode magit haskell-mode haskell-emacs use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
