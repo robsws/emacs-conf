b77e1c0e52aece0026eb0317a46c1eb5cc11df38
Rob Streeting <robsws@gmail.com>
2023-02-13 07:49:57 +0000

    Merging master

--- custom.el
+++ custom.el
@@ -4,14 +4,19 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
+<<<<<<< Updated upstream
 <<<<<<< HEAD
    '(perspective ## markdown-preview-mode terraform-mode multiple-cursors yaml-mode verb request gruvbox-theme company flycheck lsp-ui lsp-mode rust-mode python-mode magit haskell-mode haskell-emacs use-package)))
 =======
    '(dap-mode company flycheck lsp-ui lsp-mode rust-mode python-mode magit haskell-mode haskell-emacs use-package)))
 >>>>>>> 8e1c45b (Add a few more modes)
+=======
+   '(dap-mode exec-path-from-shell yasnippet rustic flycheck-rust tree-sitter-indent tree-sitter-langs tree-sitter perspective ## markdown-preview-mode terraform-mode multiple-cursors yaml-mode verb request gruvbox-theme company flycheck lsp-ui lsp-mode rust-mode python-mode magit haskell-mode haskell-emacs use-package)))
+>>>>>>> Stashed changes
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:inherit nil :extend nil :stipple nil :background "#282828" :foreground "#ebdbb2" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "nil" :family "Iosevka")))))
+ '(markdown-code-face ((t (:inherit default))))
--- init.el
+++ init.el
@@ -33,9 +33,59 @@
 (load-theme 'gruvbox-dark-medium t)
 (set-frame-font 'Iosevka nil t)
 (global-display-line-numbers-mode 1)
+(setq column-number-mode t)
 
 ;; Company mode (intellisense)
 (add-hook 'after-init-hook 'global-company-mode)
+(use-package company
+  :ensure
+  :custom
+  (company-idle-delay 0.5) ;; how long to wait until popup
+  ;; (company-begin-commands nil) ;; uncomment to disable popup
+  :bind
+  (:map company-active-map
+	      ("C-n". company-select-next)
+	      ("C-p". company-select-previous)
+	      ("M-<". company-select-first)
+	      ("M->". company-select-last)))
+
+(use-package yasnippet
+  :ensure
+  :config
+  (yas-reload-all)
+  (add-hook 'prog-mode-hook 'yas-minor-mode)
+  (add-hook 'text-mode-hook 'yas-minor-mode))
+
+;; Language server
+(use-package lsp-mode
+  :ensure
+  :commands lsp
+  :custom
+  ;; what to use when checking on-save. "check" is default, I prefer clippy
+  (lsp-rust-analyzer-cargo-watch-command "clippy")
+  (lsp-eldoc-render-all t)
+  (lsp-idle-delay 0.6)
+  ;; enable / disable the hints as you prefer:
+  (lsp-rust-analyzer-server-display-inlay-hints t)
+  (lsp-rust-analyzer-display-lifetime-elision-hints-enable "skip_trivial")
+  (lsp-rust-analyzer-display-chaining-hints t)
+  (lsp-rust-analyzer-display-lifetime-elision-hints-use-parameter-names nil)
+  (lsp-rust-analyzer-display-closure-return-type-hints t)
+  (lsp-rust-analyzer-display-parameter-hints nil)
+  (lsp-rust-analyzer-display-reborrow-hints nil)
+  :config
+  (add-hook 'lsp-mode-hook 'lsp-ui-mode))
+
+(use-package lsp-ui
+  :ensure
+  :commands lsp-ui-mode
+  :custom
+  (lsp-ui-peek-always-show t)
+;;  (lsp-ui-sideline-show-hover t)
+  (lsp-ui-doc-enable nil))
+
+;; flycheck
+(use-package flycheck :ensure)
 
 ;; Org mode
 (transient-mark-mode 1)
@@ -63,3 +113,72 @@
 (setq remote-file-name-inhibit-cache nil)
 (setq vc-handled-backends '(Git))
 (setq tramp-verbose 1)
+
+;; Rust
+(defun rustic-cargo-run-with-args ()
+  "Run 'cargo run' with arguments"
+  (interactive)
+  (rustic-cargo-run t))
+
+(use-package rustic
+  :ensure
+  :bind (:map rustic-mode-map
+              ("M-j" . lsp-ui-imenu)
+              ("M-?" . lsp-find-references)
+              ("C-c C-c l" . flycheck-list-errors)
+              ("C-c C-c a" . lsp-execute-code-action)
+              ("C-c C-c r" . lsp-rename)
+              ("C-c C-c q" . lsp-workspace-restart)
+              ("C-c C-c Q" . lsp-workspace-shutdown)
+              ("C-c C-c s" . lsp-rust-analyzer-status)
+              ("C-c C-c C-r" . rustic-cargo-run-with-args))
+  :config
+  ;; uncomment for less flashiness
+  ;; (setq lsp-eldoc-hook nil)
+  ;; (setq lsp-enable-symbol-highlighting nil)
+  ;; (setq lsp-signature-auto-activate nil)
+
+  ;; comment to disable rustfmt on save
+  (setq rustic-format-on-save t)
+  (add-hook 'rustic-mode-hook 'rk/rustic-mode-hook))
+
+(defun rk/rustic-mode-hook ()
+  ;; so that run C-c C-c C-r works without having to confirm, but don't try to
+  ;; save rust buffers that are not file visiting. Once
+  ;; https://github.com/brotzeit/rustic/issues/253 has been resolved this should
+  ;; no longer be necessary.
+  (when buffer-file-name
+    (setq-local buffer-save-without-query t))
+  (add-hook 'before-save-hook 'lsp-format-buffer nil t))
+
+;; Tree sitter
+(use-package tree-sitter
+  :config
+  (require 'tree-sitter-langs)
+  (global-tree-sitter-mode)
+  (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode))
+
+;; Debugger
+(use-package exec-path-from-shell
+  :ensure
+  :init (exec-path-from-shell-initialize))
+
+(use-package dap-mode
+  :ensure
+  :config
+  (dap-ui-mode)
+  (dap-ui-controls-mode 1)
+
+  (require 'dap-lldb)
+  (require 'dap-gdb-lldb)
+  (require 'dap-cpptools)
+  ;; installs .extension/vscode
+  (dap-gdb-lldb-setup)
+  (dap-register-debug-template
+   "Rust::LLDB Run Configuration"
+   (list :type "lldb"
+         :request "launch"
+         :name "LLDB::Run"
+	 :gdbpath "rust-lldb"
+         :target nil
+         :cwd nil)))
