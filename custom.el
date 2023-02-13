1795ac3acbcc7c65c9b1b050febfdecb9089b525
Rob Streeting <rstreeting@factset.com>
2023-02-13 10:19:28 +0000

    Add a few more libs

--- custom.el
+++ custom.el
@@ -4,15 +4,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-<<<<<<< Updated upstream
-<<<<<<< HEAD
-   '(perspective ## markdown-preview-mode terraform-mode multiple-cursors yaml-mode verb request gruvbox-theme company flycheck lsp-ui lsp-mode rust-mode python-mode magit haskell-mode haskell-emacs use-package)))
-=======
-   '(dap-mode company flycheck lsp-ui lsp-mode rust-mode python-mode magit haskell-mode haskell-emacs use-package)))
->>>>>>> 8e1c45b (Add a few more modes)
-=======
    '(dap-mode exec-path-from-shell yasnippet rustic flycheck-rust tree-sitter-indent tree-sitter-langs tree-sitter perspective ## markdown-preview-mode terraform-mode multiple-cursors yaml-mode verb request gruvbox-theme company flycheck lsp-ui lsp-mode rust-mode python-mode magit haskell-mode haskell-emacs use-package)))
->>>>>>> Stashed changes
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
