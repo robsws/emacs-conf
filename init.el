ec551e0eacc08b064e136e1cfbeb8656800d0994
Rob Streeting <rstreeting@factset.com>
2023-04-19 17:24:33 +0100

    feat(org-roam): timestamped nodes

--- init.el
+++ init.el
@@ -489,6 +489,7 @@
               (tags-todo "+wishlist")))
 
 (use-package org-roam
+
   :custom
   (org-roam-directory "~/notes/knowledge")
   (org-roam-completion-everywhere t)
@@ -496,12 +497,23 @@
    '(("d" "default" plain "%?"
       :if-new (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n#+date: %U\n")
       :unnarrowed t)))
+  (org-roam-dailies-directory "journal/")
+  (org-roam-dailies-capture-templates
+   '(("d" "default" entry "* %<%I:%M %p>: %?"
+      :if-new (file+head "%<%Y-%m-%d>.org" "#+title: %<%Y-%m-%d>\n"))))
+
   :bind (("C-c n l" . org-roam-buffer-toggle)
          ("C-c n f" . org-roam-node-find)
          ("C-c n i" . org-roam-node-insert)
          :map org-mode-map
-         ("C-M-i" . completion-at-point))
+         ("C-M-i" . completion-at-point)
+         :map org-roam-dailies-map
+         ("Y" . org-roam-dailies-capture-yesterday)
+         ("T" . org-roam-dailies-capture-tomorrow))
+  :bind-keymap
+  ("C-c n d" . org-roam-dailies-map)
   :config
+  (require 'org-roam-dailies)
   (org-roam-setup))
 
 (use-package org-download)
